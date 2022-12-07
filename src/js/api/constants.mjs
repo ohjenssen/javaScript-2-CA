d3682fbc9c2042f1b0636c5e59acfcaf717671c7
ohjenssen <oskar_jenssen@hotmail.com>
2022-12-07 12:29:35 +0100

    API links

---
+++ src/js/api/constants.mjs
@@ -0,0 +1,6 @@
+export const api_base_url = 'https://nf-api.onrender.com';
+export const api_register_url = '/api/v1/social/auth/register';
+export const api_login_url = '/api/v1/social/auth/login';
+export const api_posts_url = '/api/v1/social/posts';
+export const api_posts_query_url = '/api/v1/social/posts/?_author=true&_comments=true&_reactions=true';
+export const api_profiles_url = '/api/v1/social/profiles/'; 
